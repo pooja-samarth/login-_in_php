ac9ec6f1919dc5134fabdbc8c4ff647e0df9678a
Pranay-sadanpawar <109722552+Pranay-sadanpawar@users.noreply.github.com>
2022-07-28 14:00:50 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="txt/css" href="bootstrap.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6">
+				<h2> Login form</h2>
+				<form action="validation.php" method="post">
+
+					<div class="form-group">
+
+						<lable>username</lable>
+						<input type="text" name="user" class="form-control">
+
+						<div class="form-group">
+						<lable>password</lable>
+						<input type="password" name="password" class="form-control">
+
+
+					</div>
+					<button type="submit" class="btn btn-primary">login button</button>
+					
+				</form>
+			</div>
+				<div class="col-lg-6">
+				<h2>Sign in</h2>
+				<form action="registratiom.php" method="post">
+
+					<div class="form-group">
+
+						<lable>username</lable>
+						<input type="text" name="user" class="form-control">
+
+						<div class="form-group">
+						<lable>password</lable>
+						<input type="password" name="password" class="form-control">
+
+
+					</div>
+					<button type="submit" class="btn btn-primary">Sign in </button>
+					
+				</form>
+			    
+		        </div>
+		</div>
+
+    </div>
+    
+
+</body>
+</html>
